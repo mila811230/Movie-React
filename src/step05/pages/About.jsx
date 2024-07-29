fbae8e5e987905fb51ec31797ebcef66c2b3bc7d
mila811230 <bluegalaxy1230@gmail.com>
2024-07-29 14:28:38 +0900

    step05add

---
+++ src/step05/pages/About.jsx
@@ -0,0 +1,10 @@
+import './About.css'
+
+export function About () {
+  return (
+    <div className = 'about_container'>
+      <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam ratione quas dolorum. Beatae, similique necessitatibus quos odio sed maxime temporibus consequatur rerum veniam! Ipsum ex perferendis fugit nihil blanditiis consectetur?</span>
+      <span>영화리뷰 사이트, 2024</span>
+    </div>
+  )
+}
